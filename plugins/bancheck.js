b898269a9589d8382260dc5063fd3631e2d70cc8
WILKER OFC <developer.wilker.ofc@gmail.com>
2026-01-01 21:58:31 -0400

    Actualizar bancheck.js

--- plugins/bancheck.js
+++ plugins/bancheck.js
@@ -102,7 +102,7 @@ const handler = async (msg, { conn, text}) => {
       errMsg += "⚠️ _Error interno del servicio_";
 }
 
-    errMsg += "\n\n> Powered by: *Michi wabot*";
+    errMsg += "\n\n> Powered by: *Barboza*";
 
     await conn.sendMessage(chatID, { text: errMsg}, { quoted: msg});
     await conn.sendMessage(chatID, {
